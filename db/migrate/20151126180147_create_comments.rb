ab4d51051bd1d11c7c1ab8bfa80c13c5d9d59c20
eugen <tassadars@gmail.com>
2015-11-26 20:08:55 +0200

    Update Comments migrate
      t.references :commentable, polymorphic: true # !

--- db/migrate/20151126180147_create_comments.rb
+++ db/migrate/20151126180147_create_comments.rb
@@ -2,7 +2,8 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.text :content
-
+      t.references :commentable, polymorphic: true # !
+      
       t.timestamps null: false
     end
   end
